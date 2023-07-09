a0de263af95eb99dc4c5aa3be3ddf3599918e916
Hendri Alqori <teamhendri18@gmail.com>
2023-07-09 14:28:26 +0700

    Do testing with code

--- __tests__/clone.test.ts
+++ __tests__/clone.test.ts
@@ -1,27 +1,38 @@
-import C from '../src/main'
-import { describe, expect, it } from 'vitest'
+import C from "../src/main";
+import { describe, expect, it } from "vitest";
 
-describe('Clone', () => {
+describe("Clone", () => {
 
-  // it('Should correctly')
+  const profile = {
+    name: "Hendri",
+    department: "IT",
+    role: "Frontend Developer",
+    email: ["hendri@email.co.id", "alqori@yahoo.co.id"],
+  };
 
-  it('Should be same value', () => {
+  it("Should be work mutable array action", () => {
+    const profileState = C.clonner(profile, (draft) => {
+      draft.email.pop();
 
-    const profile = {
-      name: 'Hendri',
-      department: 'IT',
-      role: 'Frontend Engineer'
-    }
+      draft.email.unshift("een@email.co.id");
+    });
 
-    const profileState = C.clonner(profile, draft => {
-      draft.name = 'Hendri Alqori'
-    })
+    expect(profileState.email).toEqual(['een@email.co.id','hendri@email.co.id'])
+
+  });
+
+  it("Should be same value", () => {
+    const profileState = C.clonner(profile, (draft) => {
+      draft.name = "Hendri Alqori";
+      draft.department = "Information Technology";
+      draft.role = "Frontend Engineer";
+    });
 
     expect(profileState).toEqual({
-      name: 'Hendri Alqori',
-      department: 'IT',
-      role: 'Frontend Engineer'
-    })
-
-  })
-})
+      name: "Hendri Alqori",
+      department: "Information Technology",
+      role: "Frontend Engineer",
+      email: ["hendri@email.co.id", "alqori@yahoo.co.id"],
+    });
+  });
+});
